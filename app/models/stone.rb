5e6a6c9bf5cbf24a77c5c4b1745bf9fe5ca3d100
Sam Russell <sam.russell@powershop.co.nz>
2015-11-11 14:32:57 +1300

    stones cannot be placed outside the board #5

--- app/models/stone.rb
+++ app/models/stone.rb
@@ -1,6 +1,17 @@
 class Stone < ActiveRecord::Base
+  NUMBER_OF_ROWS = 19
+  NUMBER_OF_COLUMNS = 19
+
   validates :row, presence: true
+  validates :row, inclusion: {
+    in: (0...NUMBER_OF_ROWS),
+    message: "row is %{value}, it must be 0 <= value < #{NUMBER_OF_ROWS}"
+  }
   validates :column, presence: true
+  validates :column, inclusion: {
+    in: (0...NUMBER_OF_COLUMNS),
+    message: "column is %{value}, it must be 0 <= value < #{NUMBER_OF_COLUMNS}"
+  }
   validates :color, presence: true
   validates :color, inclusion: {
     in: %w(black white),
--- spec/models/stone_spec.rb
+++ spec/models/stone_spec.rb
@@ -7,6 +7,29 @@ RSpec.describe Stone, type: :model do
     it { is_expected.to be_valid }
   end
 
+  context 'with a row and a column and color, outside edges' do
+    let(:too_low) { -1 }
+    let(:too_high) { 19 }
+    let(:stones) {
+      [
+        Stone.create(row: too_low, column: 2, color: 'black'),
+        Stone.create(row: too_high, column: 2, color: 'black'),
+        Stone.create(row: 3, column: too_low, color: 'black'),
+        Stone.create(row: 3, column: too_high, color: 'black'),
+        Stone.create(row: too_low, column: too_low, color: 'black'),
+        Stone.create(row: too_high, column: too_high, color: 'black'),
+        Stone.create(row: too_high, column: too_low, color: 'black'),
+        Stone.create(row: too_low, column: too_high, color: 'black')
+      ]
+    }
+ 
+    it 'is invalid' do
+      stones.each do |stone|
+        expect(stone).to be_invalid
+      end
+    end
+  end
+
   context 'with a row and a color a no column' do
     subject(:stone) { Stone.create(row: 3, column: nil, color: 'black') }
     
