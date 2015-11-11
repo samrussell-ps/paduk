4e136bbfc44a80c858f11f98f4e1012192f1e3c8
Sam Russell <sam.russell@powershop.co.nz>
2015-11-11 16:35:03 +1300

    finds groups of connected stones

---
+++ app/queries/connected_stones.rb
@@ -0,0 +1,39 @@
+require 'set'
+
+class ConnectedStones
+  def initialize(row, column)
+    @row = row
+    @column = column
+  end
+
+  def call
+    flood_fill([@row, @column], Set.new).to_a
+  end
+
+  private
+
+  def flood_fill(coordinate, visited_coordinates)
+    if coordinate && visited_coordinates.exclude?(coordinate) && Stone.where(row: coordinate[0], column: coordinate[1]).present?
+      visited_coordinates << coordinate
+
+      [:north, :east, :south, :west].each do |direction|
+        visited_coordinates = flood_fill(move(direction, coordinate), visited_coordinates)
+      end
+    end
+
+    visited_coordinates 
+  end
+
+  def move(direction, coordinate)
+    case direction
+    when :north
+      [coordinate[0]-1, coordinate[1]]
+    when :south
+      [coordinate[0]+1, coordinate[1]]
+    when :east
+      [coordinate[0], coordinate[1]-1]
+    when :west
+      [coordinate[0], coordinate[1]+1]
+    end
+  end
+end
---
+++ spec/queries/connected_stones_spec.rb
@@ -0,0 +1,93 @@
+require 'rails_helper'
+require 'set'
+
+describe ConnectedStones do
+  def locations_to_coordinates(locations)
+    coordinates = Set.new
+
+    locations.each_with_index do |row, row_number|
+      row.each_with_index do |cell, column_number|
+        if cell == 1
+          coordinates.add([row_number, column_number])
+        end
+      end
+    end
+
+    coordinates
+  end
+
+  let(:stone_locations) { 
+    [
+      [ 1, 1, 1, 1, 1, 0, 0 ],
+      [ 1, 0, 1, 1, 1, 1, 1 ],
+      [ 1, 0, 0, 0, 1, 1, 0 ],
+      [ 1, 0, 1, 1, 0, 1, 0 ],
+      [ 1, 1, 0, 0, 1, 1, 0 ],
+      [ 1, 0, 1, 1, 1, 0, 1 ],
+      [ 1, 1, 1, 1, 1, 0, 1 ]
+    ]
+  }
+  let(:connected_stones1){
+    [
+      [ 1, 1, 1, 1, 1, 0, 0 ],
+      [ 1, 0, 1, 1, 1, 1, 1 ],
+      [ 1, 0, 0, 0, 1, 1, 0 ],
+      [ 1, 0, 0, 0, 0, 1, 0 ],
+      [ 1, 1, 0, 0, 1, 1, 0 ],
+      [ 1, 0, 1, 1, 1, 0, 0 ],
+      [ 1, 1, 1, 1, 1, 0, 0 ]
+    ]
+  }
+  let(:connected_stones2) { 
+    [
+      [ 0, 0, 0, 0, 0, 0, 0 ],
+      [ 0, 0, 0, 0, 0, 0, 0 ],
+      [ 0, 0, 0, 0, 0, 0, 0 ],
+      [ 0, 0, 0, 0, 0, 0, 0 ],
+      [ 0, 0, 0, 0, 0, 0, 0 ],
+      [ 0, 0, 0, 0, 0, 0, 1 ],
+      [ 0, 0, 0, 0, 0, 0, 1 ],
+    ]
+  }
+  let(:connected_stones3) { 
+    [
+      [ 0, 0, 0, 0, 0, 0, 0 ],
+      [ 0, 0, 0, 0, 0, 0, 0 ],
+      [ 0, 0, 0, 0, 0, 0, 0 ],
+      [ 0, 0, 1, 1, 0, 0, 0 ],
+      [ 0, 0, 0, 0, 0, 0, 0 ],
+      [ 0, 0, 0, 0, 0, 0, 0 ],
+      [ 0, 0, 0, 0, 0, 0, 0 ],
+    ]
+  }
+
+  before do
+    locations_to_coordinates(stone_locations).each do |coordinate|
+      Stone.create!(row: coordinate[0], column: coordinate[1], color: 'white')
+    end
+  end
+
+  it 'finds the easy (non-recursive) matches' do
+    easy_coordinates2 = locations_to_coordinates(connected_stones2)
+    easy_coordinates3 = locations_to_coordinates(connected_stones3)
+
+    easy_coordinates2.each do |coordinate|
+      connected_stones = ConnectedStones.new(coordinate[0], coordinate[1]).call
+      expect(connected_stones).to contain_exactly(*easy_coordinates2)
+    end
+
+    easy_coordinates3.each do |coordinate|
+      connected_stones = ConnectedStones.new(coordinate[0], coordinate[1]).call
+      expect(connected_stones).to contain_exactly(*easy_coordinates3)
+    end
+  end
+
+  it 'finds the hard (recursive) match' do
+    hard_coordinates = locations_to_coordinates(connected_stones1)
+
+    hard_coordinates.each do |coordinate|
+      connected_stones = ConnectedStones.new(coordinate[0], coordinate[1]).call
+      expect(connected_stones).to contain_exactly(*hard_coordinates)
+    end
+  end
+end
