a6f9a051cb4a658a42cc041a39655e607d08d420
Sam Russell <sam.russell@powershop.co.nz>
2015-11-26 17:03:12 +1300

    move score code into its own file

---
+++ app/assets/javascripts/score.js
@@ -0,0 +1,16 @@
+<!--
+$(function() {
+  $('input').change(function() {
+    $('#black-score').text(calculateBlackScore());
+    $('#white-score').text(calculateWhiteScore());
+  });
+});
+
+function calculateBlackScore(){
+  return parseInt($('#white-lost-stones').text()) + parseInt($('#black-territory').val());
+}
+
+function calculateWhiteScore(){
+  return parseInt($('#black-lost-stones').text()) + parseInt($('#white-territory').val());
+}
+// -->
