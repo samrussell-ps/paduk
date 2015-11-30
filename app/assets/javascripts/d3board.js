65922fe52f85079df7f6d12391b894034f312b64
Sam Russell <sam.russell@powershop.co.nz>
2015-12-01 11:38:20 +1300

    javascript refactoring

--- app/assets/javascripts/d3board.js
+++ app/assets/javascripts/d3board.js
@@ -1,6 +1,99 @@
+var Color = function(colorString) {
+  this.colorString = colorString
+};
+
+Color.prototype.to_s = function() {
+  switch(this.colorString) {
+    case "white":
+      return "#ffffff";
+    case "black":
+      return "#000000";
+  }
+  return null;
+};
+
+Color.prototype.other = function() {
+  switch(this.colorString) {
+    case "white":
+      return new Color("black");
+    case "black":
+      return new Color("white");
+  }
+  return new Color(null);
+};
+
+var Board = function() {};
+
+Board.prototype.display = function() {
+  var board = d3.select("#board-container").append("svg").attr("height", "475px").attr("width", "475px");
+  board.append("rect").attr({width: 475, height: 475, x: 0, y: 0, fill: "#E09E48"});
+
+  for(var i=0; i<19; i++){
+    board.append("line")
+    .attr({
+      x1: 12.5,
+      x2: 475 - 12.5,
+      y1: 25*i+12.5,
+      y2: 25*i+12.5,
+      stroke: "#000000",
+      "stroke-width": 1
+    });
+
+    board.append("line")
+    .attr({
+      y1: 12.5,
+      y2: 475 - 12.5,
+      x1: 25*i+12.5,
+      x2: 25*i+12.5,
+      stroke: "#000000",
+      "stroke-width": 1
+    });
+  }
+
+  [3, 9, 15].forEach(function (x){
+    [3, 9, 15].forEach(function (y){
+      board.append("ellipse")
+      .attr({
+        cx: x*25+12.5,
+        cy: y*25+12.5,
+        rx: 3,
+        ry: 3,
+        fill: "#000000"
+      });
+    });
+  });
+
+  $('svg').click(boardClick);
+
+  this.board = board;
+};
+
+Board.prototype.placeStone = function(coordinate, color) {
+  var x = coordinate["column"];
+  var y = coordinate["row"];
+
+  this.board.append("ellipse")
+  .attr({
+    cx: x*25+12.5,
+    cy: y*25+12.5,
+    rx: 9,
+    ry: 9,
+    fill: color.other().to_s()
+  });
+
+  this.board.append("ellipse")
+  .attr({
+    cx: x*25+12.5,
+    cy: y*25+12.5,
+    rx: 8,
+    ry: 8,
+    fill: color.to_s()
+  });
+};
+
 function initBoard(board_data){ 
   var board = d3.select("#board-container").append("svg").attr("height", "475px").attr("width", "475px");
-  board.append("rect").attr({width: 500, height: 500, x: 0, y: 0, fill: "#E09E48"});
+  board.append("rect").attr({width: 475, height: 475, x: 0, y: 0, fill: "#E09E48"});
 
   for(var i=0; i<19; i++){
     board.append("line")
@@ -38,41 +131,14 @@ function initBoard(board_data){
   });
 
   board_data["stones"]["black"].forEach(function(coordinate) {
-    drawStone(coordinate["column"], coordinate["row"], "#000000", board);
+    drawStone(coordinate["column"], coordinate["row"], "black", board);
   });
 
   board_data["stones"]["white"].forEach(function(coordinate) {
-    drawStone(coordinate["column"], coordinate["row"], "#ffffff", board);
+    drawStone(coordinate["column"], coordinate["row"], "white", board);
   });
 
   $('svg').click(boardClick);
-}
-
-function otherColor(color) {
-  if(color == "#ffffff"){
-    return "#000000";
-  }
-  return "#ffffff";
-}
-
-function drawStone(x, y, color, board) {
-      board.append("ellipse")
-      .attr({
-        cx: x*25+12.5,
-        cy: y*25+12.5,
-        rx: 9,
-        ry: 9,
-        fill: otherColor(color)
-      });
-
-      board.append("ellipse")
-      .attr({
-        cx: x*25+12.5,
-        cy: y*25+12.5,
-        rx: 8,
-        ry: 8,
-        fill: color
-      });
 }
 
 function submitTurn(x, y) {
@@ -85,7 +151,6 @@ function boardClick(e) {
   square = mouseCoordToSquare(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top);
   if(closeToSquare(square[0], square[1]))
     submitTurn(Math.round(square[0]), Math.round(square[1]));
-    //console.log(Math.round(square[0]) + ', ' + Math.round(square[1]));
 }
 
 function closeToSquare(x, y) {
--- spec/javascripts/d3board_spec.js
+++ spec/javascripts/d3board_spec.js
@@ -1,20 +1,74 @@
-describe("D3BoardSpec", function () {
+describe("ColorSpec", function() {
+  describe("#to_s", function() {
+    it("returns the string version", function() {
+      var whiteColor = new Color("white");
+      var blackColor = new Color("black");
+      var badColor = new Color("green");
+      
+      expect(whiteColor.to_s()).toBe("#ffffff");
+      expect(blackColor.to_s()).toBe("#000000");
+      expect(badColor.to_s()).toBe(null);
+    });
+  });
 
+  describe("#other", function() {
+    it("returns the other color", function() {
+      var whiteColor = new Color("white");
+      var blackColor = new Color("black");
+      var badColor = new Color("green");
+      
+      expect(whiteColor.other().to_s()).toBe("#000000");
+      expect(blackColor.other().to_s()).toBe("#ffffff");
+      expect(badColor.other().to_s()).toBe(null);
+
+      expect(whiteColor.other().other().to_s()).toBe("#ffffff");
+      expect(blackColor.other().other().to_s()).toBe("#000000");
+    });
+  });
+});
+
+describe("BoardSpec", function () {
+  var board;
   beforeEach(function() {
     setFixtures('<div id="board-container"></div>');
-    initBoard();
+    var board_data = {"removed_stones":{"black":0,"white":0},"stones":{"black":[{"row":3,"column":15},{"row":3,"column":3}],"white":[{"row":15,"column":3},{"row":15,"column":16}]}};
+    //initBoard(board_data);
+    board = new Board();
   });
 
-  it("creates an svg", function() {
-    expect($("#board-container").children()).toContain("svg");
+  describe("#display()", function() {
+    beforeEach(function() {
+      board.display();
+    });
+
+    it("creates an svg", function() {
+      expect($("#board-container").children()).toContain("svg");
+    });
+
+    it("creates the back of the board", function() {
+      expect($("#board-container").children("svg").children("rect").length).toBe(1);
+      expect($("#board-container").children("svg").children("rect").attr("fill")).toBe("#E09E48");
+    });
+
+    it("draws the lines", function() {
+      expect($("#board-container").children("svg").children("line").length).toBe(19*2);
+    });
   });
 
-  it("creates the back of the board", function() {
-    expect($('#board-container').children('svg').children(('rect')).length).toBe(1)
-    expect($('#board-container').children('svg').children(('rect')).attr('fill')).toBe('#E09E48')
-  });
+  describe("#placeStone()", function() {
+    beforeEach(function() {
+      board.display();
+    });
 
-  it("draws the lines", function() {
-    expect($('#board-container').children('svg').children(('line')).length).toBe(19*2)
+    it("adds a stone", function() {
+      var coordinate = { "row" : 4, "column" : 2 };
+      var color = new Color('white');
+
+      expect($("#board-container").children("svg").children("ellipse").length).toBe(9);
+
+      board.placeStone(coordinate, color);
+
+      expect($("#board-container").children("svg").children("ellipse").length).toBe(9 + 2);
+    });
   });
 });
