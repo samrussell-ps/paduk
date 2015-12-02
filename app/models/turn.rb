fb670e19f5e592a42c137797848a456fa1e1e0e6
Sam Russell <sam.russell@powershop.co.nz>
2015-12-02 14:32:48 +1300

    71 commits before catching dependent destroy xD

--- app/models/turn.rb
+++ app/models/turn.rb
@@ -1,8 +1,8 @@
 class Turn < ActiveRecord::Base
   # TODO look at keeping these the same, STI or relations
   # maybe type/kind column
-  has_many :stone_additions
-  has_many :stone_removals
+  has_many :stone_additions, dependent: :destroy
+  has_many :stone_removals, dependent: :destroy
 
   def self.ordered(id = nil)
     if id
